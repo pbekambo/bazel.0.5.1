843dc1c2bd76315e78cb03ee21bad4bbebe9e6ea
Paulin Bekambo <paulin.bekambo@emessoft.com>
2020-06-22 05:41:00 +0200

    sets the maximum size of the Java heap to 500 MB

--- scripts/bootstrap/compile.sh
+++ scripts/bootstrap/compile.sh
@@ -121,7 +121,7 @@ function java_compilation() {
 
   run "${JAVAC}" -classpath "${classpath}" -sourcepath "${sourcepath}" \
       -d "${output}/classes" -source "$JAVA_VERSION" -target "$JAVA_VERSION" \
-      -encoding UTF-8 "@${paramfile}"
+      -encoding UTF-8 "@${paramfile}" -J-Xmx500M
 
   log "Extracting helper classes for $name..."
   for f in ${library_jars} ; do
